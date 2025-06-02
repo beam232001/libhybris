acf5662b52897243ac31f410dea6be6ba2e7276f
James Freeman <beam232001@gmail.com>
2025-06-02 12:28:57 -0400

    feat: implement fallback for android_dlopen_ext

--- hybris/common/dlfcn_compat.c
+++ hybris/common/dlfcn_compat.c
@@ -1,9 +1,15 @@
 #include "dlfcn_compat.h"
 #include <dlfcn.h>
-#include "bionic_compatibility.h"
+#include <android/dlext.h>
+#include <string.h>
+#include <stdio.h>
 
-void* android_dlopen_ext(const char* filename, int flag, const void* extinfo)
+void* android_dlopen_ext(const char* filename, int flag, const android_dlextinfo* extinfo)
 {
-    // Temporary stub
+    // extinfo is optional; we ignore its fields for now
+    if (extinfo != NULL) {
+        fprintf(stderr, "android_dlopen_ext: ignoring extinfo fields (not implemented)\n");
+    }
     return dlopen(filename, flag);
 }
+
