2b33945f7f903a4bdba9136ac9f8ae89227143a3
James Freeman <beam232001@gmail.com>
2025-06-01 23:14:43 -0400

    compat: add __get_tls() macro for AOSP 15 archs

--- hybris/common/bionic_compatibility.h
+++ hybris/common/bionic_compatibility.h
@@ -24,3 +24,14 @@ extern "C" {
 #endif
 
 #ifdef __cplusplus
+
+#if defined(__aarch64__)
+#define __get_tls() ({ void** __val; __asm__("mrs %0, tpidr_el0" : "=r"(__val)); __val; })
+#elif defined(__arm__)
+#define __get_tls() ({ void** __val; __asm__("mrc p15, 0, %0, c13, c0, 3" : "=r"(__val)); __val; })
+#elif defined(__x86_64__)
+#define __get_tls() ({ void** __val; __asm__("mov %%fs:0, %0" : "=r"(__val)); __val; })
+#elif defined(__i386__)
+#define __get_tls() ({ void** __val; __asm__("movl %%gs:0, %0" : "=r"(__val)); __val; })
+#endif
+
